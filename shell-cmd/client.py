7051fc7cd4b770e16aa1c4e0029d9566a9079869
Jim Pruyne <pruyne@uchicago.edu>
2023-07-19 15:58:39 -0500

    Updated for state models
    Also, using the tool for Shell now included in the main gladier repo.

--- shell-cmd/client.py
+++ shell-cmd/client.py
@@ -2,30 +2,26 @@
 Make sure you are part of the Globus Flows Users group so that you can deploy this flow,
 or delete any prior flows before running this example.
 """
-from gladier import GladierBaseClient, generate_flow_definition
+from gladier import GladierBaseClient
+from gladier.tools.posix import ShellCmdStep
 from pprint import pprint
 
 
-@generate_flow_definition
-class ShellCmdClient(GladierBaseClient):
-    gladier_tools = [
-        "gladier_tools.posix.shell_cmd.ShellCmdTool",
-    ]
-
-
-if __name__ == "__main__":
+def main():
     flow_input = {
         "input": {
-            "args": "cat /proc/version",
-            "capture_output": True,
-            "funcx_endpoint_compute": "4b116d3c-1703-4f8f-9f6f-39921e5864df",
+            "globus_compute_endpoint": "4b116d3c-1703-4f8f-9f6f-39921e5864df",
         }
     }
-    # Instantiate the client
-    shell_cmd_client = ShellCmdClient()
+    shell_cmd_client = ShellCmdStep(
+        cmd_args="cat /proc/version",
+        capture_output=True,
+    )
+
+    shell_cmd_client = GladierBaseClient(start_at=shell_cmd_client)
 
     # Optionally, print the flow definition
-    pprint(shell_cmd_client.flow_definition)
+    pprint(shell_cmd_client.get_flow_definition())
 
     # Run the flow
     flow = shell_cmd_client.run_flow(flow_input=flow_input, label="Shell CMD Example")
@@ -34,3 +30,7 @@ if __name__ == "__main__":
     run_id = flow["run_id"]
     shell_cmd_client.progress(run_id)
     pprint(shell_cmd_client.get_status(run_id))
+
+
+if __name__ == "__main__":
+    main()
