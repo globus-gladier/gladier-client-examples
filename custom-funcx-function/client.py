4c31a0c00148bec6a932466ef3d837e5016d0e40
Jim Pruyne <pruyne@uchicago.edu>
2023-07-21 03:57:46 -0500

    Updated the custom-funcx-function example

--- custom-funcx-function/client.py
+++ custom-funcx-function/client.py
@@ -2,62 +2,57 @@
 Make sure you are part of the Globus Flows Users group so that you can deploy this flow,
 or delete any prior flows before running this example.
 """
-from gladier import GladierBaseClient, GladierBaseTool, generate_flow_definition
+from gladier import (
+    GladierBaseClient,
+    GladierBaseTool,
+    JSONObject,
+    generate_flow_definition,
+)
+from gladier.tools.globus import GlobusComputeStep
 from pprint import pprint
+import typing as t
 
 
 def compute_sum(a: int, b: int, **data) -> int:
     return a + b
 
 
-@generate_flow_definition
-class ComputeSum(GladierBaseTool):
-    """
-    Compute two numbers and return the result. Requires the following flow input:
+class ComputeSumStep(GlobusComputeStep):
+    function_to_call: t.Union[t.Callable[[t.Any], t.Any], str] = compute_sum
+    a: t.Union[
+        str, int
+    ] = 2  # By default, a will have a value of '2' if not specified as input
+    b: t.Union[str, int] = "$.input.b"
 
-    a: (optional, defaults to 2): first integer to add
-    b: second integer to add
-    funcx_endpoint_compute: Funcx endpoint used to compute these values
-    returns: the computed result
-    """
-
-    funcx_functions = [compute_sum]
-    required_input = [
-        "a",
-        "b",
-        "funcx_endpoint_compute",
-    ]
-    # By default, a will have a value of '2' if not specified as input
-    flow_input = {"a": 2}
-
-
-@generate_flow_definition
-class ComputeSumClient(GladierBaseClient):
-    gladier_tools = [
-        ComputeSum,
-    ]
+    def get_flow_definition(self) -> JSONObject:
+        # The model parameters a and b will be passed as arguments to the function to invoke
+        self.set_call_params_from_self_model(["a", "b"])
+        return super().get_flow_definition()
 
 
 if __name__ == "__main__":
     flow_input = {
         "input": {
             # 'a': 3,
-            # 'b': 2,
-            "funcx_endpoint_compute": "4b116d3c-1703-4f8f-9f6f-39921e5864df",
+            "b": 4,
+            "globus_compute_endpoint": "4b116d3c-1703-4f8f-9f6f-39921e5864df",
         }
     }
+
+    compute_sum_step = ComputeSumStep()
+
     # Instantiate the client
-    shell_cmd_client = ComputeSumClient()
+    compute_sum_client = GladierBaseClient(start_at=compute_sum_step)
 
     # Optionally, print the flow definition
-    pprint(shell_cmd_client.flow_definition)
+    pprint(compute_sum_client.get_flow_definition())
 
     # Run the flow
-    flow = shell_cmd_client.run_flow(
-        flow_input=flow_input, label="Custom FuncX Func Example"
+    flow = compute_sum_client.run_flow(
+        flow_input=flow_input, label="Custom Globus Compute Func Example"
     )
 
     # Track the progress
     run_id = flow["run_id"]
-    shell_cmd_client.progress(run_id)
-    pprint(shell_cmd_client.get_status(run_id))
+    compute_sum_client.progress(run_id)
+    pprint(compute_sum_client.get_status(run_id))
